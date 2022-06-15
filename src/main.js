a5083f13f53aded1c0bac04e6c648fbd69b6acf6
vinkim <928287706@qq.com>
2022-06-15 17:15:22 +0800

    添加element icon组件和登录页页面布局

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import request from './util/request.js';
 import storage from './util/storage.js';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App);
+for (let iconName in ElementPlusIconsVue) {
+    app.component(iconName, ElementPlusIconsVue[iconName])
+  }
 app.config.globalProperties.$request = request;
 app.config.globalProperties.$storage = storage;
 app.use(router).use(ElementPlus).mount('#app');
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,8 +5,47 @@ export default {
 </script>
 
 <template>
-  <h1>Login</h1>
+  <div class="login-wrapper">
+    <div class="model">
+        <el-form>
+          <div class="title">火星</div>
+          <el-form-item >
+            <el-input type="text" placeholder="请输入账号" prefix-icon="avatar" />
+          </el-form-item>
+          <el-form-item >
+            <el-input type="password" placeholder="请输入密码" prefix-icon="search" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" class="btn-login">登录</el-button>
+          </el-form-item>
+        </el-form>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
+.login-wrapper{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f9fcff;
+  width:100vw;
+  height:100vh;
+  .model{
+    width:500px;
+    padding:50px;
+    background-color:#fff;
+    border-radius: 4px;
+    box-shadow: 0px 0px 10px 3px #c7c9cb4d;
+    .title{
+      font-size:50px;
+      line-height: 1.5;
+      text-align: center;
+      margin-bottom: 30px;
+    }
+    .btn-login{
+      width:100%;
+    }
+  }
+}
 </style>
