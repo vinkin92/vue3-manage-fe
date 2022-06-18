805031f93ec1766ea8f958c4ac8f5367a0e1ebaa
vinkim <928287706@qq.com>
2022-06-18 12:00:02 +0800

    欢迎页面静态布局

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -5,9 +5,46 @@ export default {
 </script>
 
 <template>
-  <h1>Welcome</h1>
-  <router-link to="/login">登录页</router-link>
+  <div class="welcome">
+    <div class="content">
+      <div class="sub-title">欢迎体验</div>
+      <div class="title">慕课通用后台管理系统</div>
+      <div class="desc">- Vue3.0 + ElementPlus + Node + Mongo 打造通用后台管理系统</div>
+    </div>
+    <div class="img"></div>
+  </div>
 </template>
 
 <style lang="scss">
+.welcome{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height:100%;
+  .content{
+    position: relative;
+    bottom:40px;
+    .sub-title{
+      font-size: 30px;
+      line-height: 42px;
+      color:#333;
+    }
+    .title{
+      font-size: 40px;
+      line-height: 62px;
+      color:#409eff;
+    }
+    .desc{
+      text-align: right;
+      font-size:14px;
+      color:#999;
+    }
+  }
+  .img{
+    width:371px;
+    height:438px;
+    background-image: url('./../assets/images/welcome.png');
+    margin-left: 105px;
+  }
+}
 </style>
