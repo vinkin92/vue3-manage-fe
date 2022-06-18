95758845f67bfdc15db76829e3cc8462e90588af
vinkim <928287706@qq.com>
2022-06-18 11:46:31 +0800

    首页左右布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,9 +5,51 @@ export default {
 </script>
 
 <template>
-  <h1>欢迎学习 Vue3 全栈课程</h1>
-  <router-view></router-view>
+  <div class="basic-layout">
+    <div class="nav-side">123</div>
+    <div class="content-right">
+      <div class="nav-top">
+        <div class="bread">面包屑</div>
+        <div class="userInfo">admin</div>
+      </div>
+      <div class="wrapper">
+        <div class="main-page"><router-view></router-view></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
+.basic-layout{
+  position: relative;
+  .nav-side{
+    position:fixed;
+    width:200px;
+    height:100vh;
+    background-color: #001529;
+    color:#fff;
+    overflow-y: auto;
+    transition: width .5s
+  }
+  .content-right{
+    margin-left: 200px;
+    .nav-top{
+      height: 50px;
+      line-height: 50px;
+      display: flex;
+      justify-content: space-between;
+      border-bottom: 1px solid #ddd;
+      padding:0 20px;
+    }
+    .wrapper{
+      background-color: #eef0f3;
+      padding: 20px;
+      height:calc(100vh - 50px);
+      .main-page{
+        background-color: #fff;
+        height:100vh;
+      }
+    }
+  }
+}
 </style>
