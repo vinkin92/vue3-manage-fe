5dcd641e247ea4931ea7e4acd7daf3aabe4b3831
vinkim <928287706@qq.com>
2022-06-22 16:51:40 +0800

    点击新增用户弹窗的基本布局

--- src/views/User.vue
+++ src/views/User.vue
@@ -42,6 +42,17 @@ export default {
         //选中用户列表的对象
         const checkdUserIds = ref([])
         const formRef = ref()
+        // 是否显示弹窗添加新用户
+        const dialogVisible = ref(false)
+        // 新增用户Form 对象
+        const userForm = reactive({state:3})
+        // 新增用户表校验规则
+        const rules = reactive({
+            userName:[{required:true,message:"请输入用户名称",trigger:'blur'}],
+            userEmail:[{required:true,message:"请输入邮箱",trigger:'blur'}],
+            mobile:[{required:true,message:"请输入邮箱",trigger:'blur'},{pattern:/1\d{10}/,message:'请输入正确的手机号',trigger:'blur'},{type:'number',message:'输入类型必须为数字',trigger:'blur'}],
+            deptId:[{required:true,message:"请输入邮箱",trigger:'blur'}]
+        });
         onMounted(()=>{
             getUserList()
         })
@@ -93,7 +104,7 @@ export default {
             checkdUserIds.value = arr;
         }
         return {user,userList,column,getUserList,pager,handleQuery,handleReset,formRef,handleCurrentChange,handleDel,checkdUserIds,handlePatchDel,
-            handleSelectionChange
+            handleSelectionChange,dialogVisible,userForm,rules
         }
     }
 };
@@ -123,9 +134,10 @@ export default {
             </el-form-item>
         </el-form>
     </div>
+    <!--  用户列表 -->
     <div class="base-table">
         <div class="action">
-            <el-button type="primary">新增</el-button>
+            <el-button type="primary" @click="dialogVisible = true">新增</el-button>
             <el-button type="danger" @click="handlePatchDel">批量删除</el-button>
         </div>
         <el-table :data="userList" @selection-change="handleSelectionChange">
@@ -140,8 +152,54 @@ export default {
         </el-table>
         <el-pagination background layout="prev, pager, next" :total="pager.total" class="pagination" :page-size="pager.pageSize" @current-change="handleCurrentChange"/>
     </div>
+    <!-- 弹窗添加用户 -->
+    <el-dialog v-model="dialogVisible" title="用户新增">
+        <el-form :model="userForm" label-width="80px" ref="DialogForm" :rules="rules">
+            <el-form-item label="用户名" prop="userName">
+                <el-input v-model="userForm.userName" placeholder="请输入用户名称"/>
+            </el-form-item>
+            <el-form-item label="邮箱" prop="userEmail">
+                <el-input v-model="userForm.userEmail" placeholder="请输入用户邮箱">
+                    <template #append>
+                        @imooc.com
+                    </template>
+                </el-input>
+            </el-form-item>            
+            <el-form-item label="手机号" prop="mobile">
+                <el-input v-model="userForm.mobile" placeholder="请输入手机号"/>
+            </el-form-item>            
+            <el-form-item label="岗位" prop="job">
+                <el-input v-model="userForm.job" placeholder="请输入岗位"/>
+            </el-form-item>            
+            <el-form-item label="状态" prop="state">
+                <el-select v-model="userForm.state">
+                    <el-option :value="1" label="在职"></el-option>
+                    <el-option :value="2" label="离职"></el-option>
+                    <el-option :value="3" label="试用期"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="系统角色" prop="roleList">
+                <el-select v-model="userForm.roleList" placeholder="请选择用户系统角色">
+                    <el-option></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="部门" prop="deptId">
+                <!-- 显示的值为label ，提交的值为value ,服务器返回的是_id和deptName,所以使用组件自带的转换工具把 _id替换成value,deptName自动替换成label -->
+                <el-cascader v-model="userForm.deptId" :options="options" :props="{checkStrictly:true,value:'_id',label:'deptName'}" clearable placeholder="请选择所属部门" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="dialogVisible = false">取消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确定</el-button>
+            </span>
+        </template>
+    </el-dialog>
   </div>
 </template>
 
 <style lang="scss">
+.dialog-footer button:first-child {
+  margin-right: 10px;
+}
 </style>
